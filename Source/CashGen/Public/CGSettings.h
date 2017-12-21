909308384d516d731b60dbe843d0b9f1f18dec59
Chris Ashworth <chris@cashworth.net>
2017-12-21 13:06:41 +0100

    Added settings file

---
+++ Source/CashGen/Public/CGSettings.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CGSettings.generated.h"
+
+
+/**
+* Implements the settings for the CashGen Plugin
+*/
+UCLASS(config = CashGen, BlueprintType)
+class UCGSettings : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(config, EditAnywhere, BlueprintReadOnly, Category = Debug)
+	bool ShowTimings = false;
+
+};
